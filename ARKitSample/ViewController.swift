62b5990f194d2f01980574e270dd2af8753a31fc
Denis Malykh <mrdekk@yandex-team.ru>
2018-02-04 18:20:32 +0500

    Initial revision

---
+++ ARKitSample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ARKitSample
+//
+//  Created by Denis Malykh on 04.02.18.
+//  Copyright © 2018 Yandex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private 
+
+}
+
